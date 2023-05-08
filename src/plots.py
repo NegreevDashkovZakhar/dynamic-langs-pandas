9c90b16d95c93edf3e1733dfb5e280e94a7f7b97
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-05-08 17:08:32 +0600

    [+] Added task 2 plot

--- src/plots.py
+++ src/plots.py
@@ -4,12 +4,15 @@ import numpy as np
 
 df = pd.read_csv("games.csv")
 
-df["moves_made"] = df["moves"].apply(lambda moves: int(np.floor(len(moves.split(' '))/2)))
+df["moves_made"] = df["moves"].apply(
+    lambda moves: int(np.floor(len(moves.split(' '))/2)))
 df["opening_name"] = df["opening_name"].apply(lambda name: name.split(":")[0])
+df["avg_rating"] = (df["white_rating"]+df["black_rating"])/2
 
 # 1.1
 openings = df["opening_name"].value_counts().head(5)
-plt.bar(openings.index,openings)
+top_opening_names = openings.index
+plt.bar(top_opening_names, openings)
 plt.show()
 
 
@@ -42,9 +45,18 @@ plt.scatter(moves_to_win.index, moves_to_win)
 
 plt.subplot(224)
 moves_to_win = df[df["winner"] == "black"]["moves_made"].value_counts()
-plt.scatter(moves_to_win.index, moves_to_win )
+plt.scatter(moves_to_win.index, moves_to_win)
 
 plt.show()
 
 # 2
 
+df[df["rated"] == True].plot(
+    x="white_rating", y="black_rating", kind='scatter')
+plt.show()
+
+df["moves_made"].plot(kind='kde')
+plt.show()
+
+df.boxplot(column=["avg_rating"], by=["opening_ply"])
+plt.show()
